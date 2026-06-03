6ee98ac8da3e21ef0599d8c2cb1592efbc677106
mayados <mariannedos.cours@gmail.com>
2026-06-03 21:38:24 +0200

    add coverage to jest

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,15 @@ import type { Config } from "jest";
 const config: Config = {
   preset: "ts-jest",
   testEnvironment: "node",
+  collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      branches: 25,
+      functions: 60,
+      lines: 45,
+      statements: 45
+    }
+  },
   testMatch: ["<rootDir>/tests/**/*.test.ts"],
   moduleFileExtensions: ["ts", "js"],
   clearMocks: true,
