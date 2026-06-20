daeca65ef1118b23ccc932956ed986fa398d902f
Mickael Joly <jolymickael67340@gmail.com>
2026-06-20 11:30:05 +0200

    feat: manifeste de compatibilité

--- gateway/tests/Endtoend.test.ts
+++ gateway/tests/Endtoend.test.ts
@@ -6,7 +6,7 @@ import app from "../src/app";
 
 const agent = request.agent(app);
 const API_VERSION = "/v1";
-// Test manifest de compatibilité
+// Test de compatabilité 20.06.2026
 // Création projet
 test("POST /projects persists project in DB", async () => {
   const login = await agent
