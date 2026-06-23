d38e2a0f254689faf536fe5838f3163180790716
Mickael Joly <jolymickael67340@gmail.com>
2026-06-23 15:31:02 +0200

    feat: manifest compatibilité

--- gateway/src/app.ts
+++ gateway/src/app.ts
@@ -19,6 +19,7 @@ declare module "express-session" {
 // Test 12h36
 // TEst 12h49
 // test 13h33
+// Test 15h30
 
 const AUTH_URL = process.env.AUTH_URL || "http://auth:3001";
 const TASKS_URL = process.env.TASKS_URL || "http://tasks:3002";
