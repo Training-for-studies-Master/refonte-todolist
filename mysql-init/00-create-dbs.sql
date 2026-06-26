ef35375091bf5564598448685c2f5d1ee7f93c93
Mickael Joly <jolymickael67340@gmail.com>
2026-06-26 23:36:26 +0200

    fix: deploiement

--- mysql-init/00-create-dbs.sql
+++ mysql-init/00-create-dbs.sql
@@ -3,3 +3,12 @@ CREATE DATABASE IF NOT EXISTS task_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unic
 CREATE DATABASE IF NOT EXISTS project_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 CREATE DATABASE IF NOT EXISTS notification_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 CREATE DATABASE IF NOT EXISTS notification_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Attribution des privilèges à l'utilisateur pour chaque base
+GRANT ALL PRIVILEGES ON auth_db.* TO 'app_user'@'%';
+GRANT ALL PRIVILEGES ON task_db.* TO 'app_user'@'%';
+GRANT ALL PRIVILEGES ON project_db.* TO 'app_user'@'%';
+GRANT ALL PRIVILEGES ON notification_db.* TO 'app_user'@'%';
+
+-- Application des nouveaux droits
+FLUSH PRIVILEGES;
