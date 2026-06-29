68a575f329a2877674c368e3f19022d21275098a
mayados <mariannedos.cours@gmail.com>
2026-06-29 12:10:04 +0200

    refactor: remove legacy backend folder

--- backend/src/routes/getItems.ts
+++
@@ -1,7 +0,0 @@
-module.exports = (db: any) => {
-    return async (req: any, res: any) => {
-        const userId = req.session.userId;
-        const items = await db.getItems(userId);
-        res.send(items);
-    };
-};
