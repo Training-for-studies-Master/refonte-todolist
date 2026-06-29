fcd67a751974db920db6cdf269a1e0723bdbfe15
Mickael Joly <jolymickael67340@gmail.com>
2026-06-29 14:14:41 +0200

    feat: implement integration test

--- gateway/src/app.ts
+++ gateway/src/app.ts
@@ -16,7 +16,6 @@ const AUTH_URL = process.env.AUTH_URL || "http://auth:3001";
 const TASKS_URL = process.env.TASKS_URL || "http://tasks:3002";
 const PROJECTS_URL = process.env.PROJECTS_URL || "http://projects:3003";
 const API_VERSION = "/v2";
-const SERVICE_VERSION = "v1";
 
 const app = express();
 app.use(express.json());
