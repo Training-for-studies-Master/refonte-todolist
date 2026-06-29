d5dfb42305b79ea9baddbefe91e803505cccc7fe
mayados <115099504+mayados@users.noreply.github.com>
2026-06-30 00:19:56 +0200

    remove legacy backend folder and it links in cd-docker and ci (#303)

--- backend/src/routes/deleteItem.ts
+++
@@ -1,8 +0,0 @@
-module.exports = (db: any) => {
-    return async (req: any, res: any) => {
-        const userId = req.session.userId;
-        await db.removeItem(req.params.id, userId);
-
-        res.sendStatus(200);
-    };
-};
